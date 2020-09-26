7850486764cb0a6e24f79aa1dd5f7ca4cd9d4440
Andreas Jirenius <ajirenius@gmail.com>
2020-09-26 15:01:14 +0200

    Added basic popup functionality

---
+++ Assets/Scripts/JCore/Example/UI/Scripts/Popup1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JCore.UI
+{
+    public class SecondScreen : Screen<NoParams>
+    {
+        public void OnButton1Press()
+        {
+            ScreenManager.Instance.Back();
+        }
+
+        private void OnDialogueResult(DialogueResult result)
+        {
+            Debug.Log("RESULT:"+result.cont);
+            if (result.cont == true)
+            {
+                
+            }
+            else
+            {
+                
+            }
+        }
+
+        public void OnButton2Press()
+        {
+            Gem.Register<DialogueResult>(OnDialogueResult);
+            ScreenManager.Instance.AddToQueue("Popup1");
+        }
+    }
+}
---
+++ Assets/Scripts/JCore/Example/UI/Scripts/Popup1Params.cs
@@ -0,0 +1,14 @@
+﻿using JCore.UI;
+
+public class FirstScreenParams : AViewParams
+{
+    public string name = "FirstScreen";
+    public int number = 12345;
+    
+    public FirstScreenParams(string name, int number)
+    {
+        this.name = name;
+        this.number = number;
+    }
+
+}
